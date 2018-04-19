d9159b0e89b94c47a4b56f26fc166dfbf9b134bc
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-19 02:31:19 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index (string)
 end
 
 def move (board, index, value = "X")
-  # board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   board[index] = value
 end
